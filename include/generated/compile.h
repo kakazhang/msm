bb12d4c11faea17b9a36b98017051bb7a289efe8
zhanglin <zhanglin3406@gmail.com>
2017-06-07 21:53:02 +0800

    kernel from qcom 8994

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Thu May 11 11:06:51 CST 2017"
+#define LINUX_COMPILE_BY "kakazhang"
+#define LINUX_COMPILE_HOST "kakazhang"
+#define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
